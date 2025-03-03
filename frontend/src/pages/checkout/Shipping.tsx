9642eba12b4991ec6ca296e741bbe10eae78d2ad
carolina <ci.arayagonzalez@gmail.com>
2025-03-03 19:50:14 -0300

    Add shipping form

--- frontend/src/pages/checkout/Shipping.tsx
+++ frontend/src/pages/checkout/Shipping.tsx
@@ -1,4 +1,5 @@
 import Stripe from "../../components/layout/header/stripePromotion/Stripe"
+import ShippingForm from "../../features/checkout/formsCheckout/ShippingForm"
 import ResumeCheckout from "../../features/checkout/resumeCheckout/ResumeCheckout"
 function Shipping() {
   return (
@@ -7,7 +8,8 @@ function Shipping() {
 
     <div className="container-fluid d-flex">
       <div className="col-6 border border-black">
-        <h1>Información de entrega</h1>
+        <h2>Datos de entrega</h2>
+        <ShippingForm/>
         <h1>Modo de Pago</h1>
       </div>
       <div className="col-6 border border-black">
