6e8e2419888949dab24c0d58739ca6eb3e2e6f54
carolina <ci.arayagonzalez@gmail.com>
2025-04-30 22:39:32 -0400

    :bug: Fix - buttons cart path

--- src/store/blocks/minicart/buttonsCart/ButtonBuy.tsx
+++ src/store/blocks/minicart/buttonsCart/ButtonBuy.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import useOffcanvas from "../../../../hooks/useOffCanvas";
+import useOffcanvas from "../../../../hooks/useOffcanvas";
 
 function ButtonBuy() {
   const { closeOffcanvas } = useOffcanvas();
