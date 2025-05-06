d9e31b0c5a61e872f57f450badce0110b82b5075
carolina <ci.arayagonzalez@gmail.com>
2025-05-06 16:13:34 -0400

    :lipstick: Add border ProductSummary PDP

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,16 +1,25 @@
-import { useParams } from 'react-router-dom'
-import { products } from '../data/productSummaryData';
-import ProductSummary from "../components/ui/ProductSummary/ProductSummary"
+import { useParams } from "react-router-dom";
+import { products } from "../data/productSummaryData";
+import ProductSummary from "../components/ui/ProductSummary/ProductSummary";
+import Header from "../store/blocks/header/Header";
+import Footer from "../store/blocks/footer/Footer";
 
 function ProductPage() {
-    const { productId } = useParams<{ productId: string }>();
-    const product = products.find((product) => product.id === Number(productId));
-    if (!product) {
-        return <h1>Product not found</h1>;
-    }
+  const { productId } = useParams<{ productId: string }>();
+  const product = products.find((product) => product.id === Number(productId));
+  if (!product) {
+    return <h1>Product not found</h1>;
+  }
   return (
-    <ProductSummary product={product} variant="pdp" />
-  )
+    <>
+      <Header />
+      <div className="global-page__wrapper">
+        <ProductSummary product={product} variant="pdp" />
+      </div>
+
+      <Footer />
+    </>
+  );
 }
 
-export default ProductPage
+export default ProductPage;
