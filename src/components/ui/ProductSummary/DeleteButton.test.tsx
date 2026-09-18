fa4cf1af71705544c70e3fa63014f7b74c988ace
carolina <ci.arayagonzalez@gmail.com>
2026-09-17 22:40:03 -0300

    fix(tests): update useCart mocks

--- src/components/ui/ProductSummary/DeleteButton.test.tsx
+++ src/components/ui/ProductSummary/DeleteButton.test.tsx
@@ -4,7 +4,7 @@ import DeleteButton from "./DeleteButton";
 
 const mockDispatch = jest.fn();
 
-jest.mock("../../../api/cart/useCart", () => ({
+jest.mock("../../../context/cart/useCart", () => ({
   __esModule: true,
   default: () => ({
     dispatch: mockDispatch,
